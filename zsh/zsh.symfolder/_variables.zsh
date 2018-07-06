57533833f9907a8906298a3d584d370be144abac
Esteban Torres <me@estebantorr.es>
2018-07-06 23:09:04 +0200

    Fix Go paths and default editor

--- zsh/zsh.symfolder/_variables.zsh
+++ zsh/zsh.symfolder/_variables.zsh
@@ -1,12 +1,21 @@
 # Export Variables
 export ZSH=$HOME/.oh-my-zsh
-export GOPATH=/usr/local/opt/go
-export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOROOT/bin
 export PATH=/usr/local/bin:$PATH
 export PATH=/usr/bin/gcov:$PATH
 export PATH=/usr/local/bin/mergepbx:$PATH
 export PATH=$(brew --prefix ruby)/bin:$PATH
-export PATH=$HOME/Documents/SoundCloud/Code/SoundCloud.nosync/ios-repos/ios/scripts/bin:$PATH
+export ZSH_PLUGINS_ALIAS_TIPS_EXCLUDES="_ g"
+
+export FPATH=$FPATH:/usr/share/zsh/site-functions:/usr/local/Cellar/zsh/5.4.1/share/zsh/functions/
+
+# Add qt to the path
+export PATH="$(brew --prefix qt@5.5)/bin:$PATH"
+
+export GOPATH=~/Documents/SoundCloud/workspace.nosync/
+export PATH=$PATH:/usr/local/bin/go
+export PATH=$PATH:$GOPATH/bin
+export PATH=$HOME/Documents/SoundCloud/workspace.nosync/src/github.com/soundcloud/ios/scripts/bin:$PATH
 
 # Ruby exec path
 export PATH=$HOME/.gem/ruby/2.3.0/bin:$PATH
@@ -16,14 +25,14 @@ export PATH=$HOME/Library/Python/2.7/bin:${PATH}
 
 export LC_ALL"=en_US.UTF-8"
 export LANG="en_US.UTF-8"
-export EDITOR="atom"
+export EDITOR="vi"
 export TERM=xterm-256color
 # Disable sending stats to speed up `pod install`
 # https://twitter.com/zadr/status/705092258152878080
 export COCOAPODS_DISABLE_STATS=1
 
 # HOMEBREW
-export HOMEBREW_EDITOR="mvim"
+export HOMEBREW_EDITOR="vi"
 # Specify your defaults in this environment variable
 #export HOMEBREW_CASK_OPTS="--appdir=/Applications --caskroom=/usr/local/Caskroom"
 
