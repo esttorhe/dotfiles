37c7d6caa157eda8178868d21c9971ee30c016a3
Esteban Torres <me@estebantorr.es>
2018-10-31 15:51:22 +0100

    Add fuzzy autocomplete to checked out branches

--- zsh/zsh.symfolder/aliases.zsh
+++ zsh/zsh.symfolder/aliases.zsh
@@ -43,4 +43,13 @@ alias rmorig='rm -rf **/**.orig'
 
 alias scknife='crun -i -o "-e USER=${USER} -v ${HOME}/.chef:${HOME}/.chef" system:latest -- bash'
 
+# sbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
+sbr() {
+  local branches branch
+  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
 # vim: ft=muttrc
