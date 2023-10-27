8a15974bc31b4636d49e2ce233dfa5c65e07bd0c
Esteban Torres <me+github@estebantorr.es>
2023-10-27 18:44:34 +0200

    Adds ripgrep config

---
+++ nix-modules/ripgrep.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+{
+  home = {
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'file'.
+    file = {
+        ".config/ripgrepcr".source = ./ripgrep/ripgrepcr;
+    };
+  };
+}
