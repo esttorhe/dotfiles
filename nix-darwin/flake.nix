5638e289bd3cd024120576fda23bc1a6ebaa8836
Esteban Torres <me+github@estebantorr.es>
2025-01-15 07:47:48 +0100

    chore(deps): upgrade dependencies

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -8,14 +8,14 @@
 
   inputs = {
     # Package sets
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.11-darwin";
     nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
 
     # Environment/system management
-    darwin.url = "github:lnl7/nix-darwin";
+    darwin.url = "github:lnl7/nix-darwin/nix-darwin-24.11";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
     flox.url = "github:flox/flox/v1.3.0";
