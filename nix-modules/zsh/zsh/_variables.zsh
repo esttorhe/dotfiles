40d1ec1bbccd882231d2c3ba3560f91134eff477
Esteban Torres <me+github@estebantorr.es>
2025-01-17 09:30:24 +0100

    chore(deps): upgrade dependencies

--- nix-modules/zsh/zsh/_variables.zsh
+++ nix-modules/zsh/zsh/_variables.zsh
@@ -1,4 +1,8 @@
 # Export Variables
+
+# Podman
+export PATH=/opt/podman/bin:$PATH
+
 #export ZSH=$HOME/.oh-my-zsh
 export PATH=/usr/local/bin:$PATH
 export PATH=/usr/bin/gcov:$PATH
@@ -39,8 +43,5 @@ export DOCKER_ENABLE_DEPRECATED_PULL_SCHEMA_1_IMAGE=true
 
 # 1Password
 export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
-
-# Podman
-export PATH="/opt/podman/bin:$PATH"
 
 # vim: ft=muttrc
