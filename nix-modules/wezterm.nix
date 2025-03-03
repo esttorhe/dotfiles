38e0f48b174d7bd88d41527fb43aaf125209fb43
Esteban Torres <me+github@estebantorr.es>
2025-03-03 11:43:32 +0100

    chore(deps): remove wezterm mapping file

--- nix-modules/wezterm.nix
+++ nix-modules/wezterm.nix
@@ -5,8 +5,6 @@
     file = {
       # Place the wezterm configuration file in the expected location
       ".wezterm.lua".source = ./wezterm/wezterm.lua;
-      #".theme.lua".source = ./wezterm/theme.lua;
-      ".mappings.lua".source = ./wezterm/mappings.lua;
     };
   };
 
