e12c27f326bd5ae141c23899fe65c7ea3e81b5e2
Esteban Torres <me+github@estebantorr.es>
2025-03-04 21:23:07 +0100

    fix(nvim): configure neovim

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -8,3 +8,30 @@ vim.opt.statuscolumn = "%@SignCb@%s%=%T%@NumCb@%l %r│%T"
 vim.wo.relativenumber = false
 
 vim.g.vim_markdown_folding_disabled = 1
+
+local homedir = os.getenv("HOME")
+
+vim.opt.spelllang = { "en" }
+
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+vim.g.vim_markdown_folding_disabled = 1
+
+vim.wo.wrap = true
+vim.wo.linebreak = true
+vim.wo.list = false
+
+vim.opt.list = false
+vim.opt.listchars = "tab:>·,trail:~,extends:>,precedes:<,space:␣,eol:¬"
+
+vim.opt.colorcolumn = "79,119"
+
+-- vim.opt.number = true
+-- vim.opt.relativenumber = true
+-- vim.opt.signcolumn = "number"
+vim.opt.statuscolumn = "%@SignCb@%s%=%T%@NumCb@%l %r│%T"
+
+if vim.g.neovide then
+  vim.o.guifont = "FiraCode Nerd Font Propo:h20"
+end
