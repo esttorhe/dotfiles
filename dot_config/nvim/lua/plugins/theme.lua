5de141b1ddecfa3d9684a014f768f1b947c1d05d
Esteban Torres <me+github@estebantorr.es>
2025-03-04 21:23:07 +0100

    fix(nvim): configure neovim

---
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "cyberdream",
+    },
+  },
+}
