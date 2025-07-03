f2cee34a04ca3e1bb362645e003db819302ebd0f
Esteban Torres <me+github@estebantorr.es>
2025-07-03 23:47:38 +0200

    feat(nvim): configure copilot

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,58 +1,72 @@
+-- lua/plugins/copilot.lua
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
+  "github/copilot.vim",
+  lazy = false, -- Load immediately rather than on keystroke
   config = function()
-    require("copilot").setup({
-      panel = {
-        enabled = true,
-        auto_refresh = true,
-        keymap = {
-          jump_prev = "[[",
-          jump_next = "]]",
-          accept = "<CR>",
-          refresh = "gr",
-          open = "<M-CR>",
-        },
-        layout = {
-          position = "bottom", -- | top | left | right
-          ratio = 0.4,
-        },
-      },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-          accept = "<Tab>",
-          accept_word = false,
-          accept_line = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
-        },
-      },
-      filetypes = {
-        yaml = true,
-        markdown = false,
-        help = true,
-        gitcommit = true,
-        gitrebase = true,
-        hgcommit = true,
-        svn = false,
-        cvs = false,
-        ["."] = false,
-      },
-      copilot_node_command = "node", -- Node.js version must be > 16.x
-      server_opts_overrides = {
-        trace = "verbose",
-        settings = {
-          advanced = {
-            listCount = 10, -- Number of completions for panel
-            inlineSuggestCount = 3, -- Number of inline completions
-          },
-        },
-      },
+    -- Basic configuration
+    vim.g.copilot_no_tab_map = true -- Disable the tab mapping
+    vim.g.copilot_assume_mapped = true
+    vim.g.copilot_tab_fallback = ""
+
+    -- Filetypes configuration - enable/disable for specific filetypes
+    vim.g.copilot_filetypes = {
+      ["*"] = true, -- Enable for all filetypes by default
+      ["markdown"] = false, -- Disable for markdown
+      ["text"] = false, -- Disable for text files
+      ["help"] = false, -- Disable for help files
+      ["gitcommit"] = false, -- Disable for git commit messages
+      ["TelescopePrompt"] = false,
+      ["DressingInput"] = false,
+    }
+
+    -- Custom keymappings for accepting suggestions
+    vim.keymap.set("i", "<C-j>", 'copilot#Accept("<CR>")', {
+      silent = true,
+      expr = true,
+      script = true,
+      replace_keycodes = false,
+    })
+
+    -- Additional keymappings for navigating suggestions
+    vim.keymap.set("i", "<C-l>", 'copilot#Accept("<Right>")', {
+      silent = true,
+      expr = true,
+      script = true,
+      replace_keycodes = false,
+    })
+    vim.keymap.set("i", "<M-]>", "<Plug>(copilot-next)", { silent = true })
+    vim.keymap.set("i", "<M-[>", "<Plug>(copilot-previous)", { silent = true })
+    vim.keymap.set("i", "<M-\\>", "<Plug>(copilot-suggest)", { silent = true })
+    vim.keymap.set("i", "<C-\\>", "<Cmd>Copilot panel<CR>", { silent = true })
+
+    -- Setup autocommands for Copilot
+    vim.api.nvim_create_augroup("copilot_settings", { clear = true })
+
+    -- Auto-enable when entering insert mode
+    vim.api.nvim_create_autocmd("InsertEnter", {
+      group = "copilot_settings",
+      callback = function()
+        if vim.g.copilot_enabled == false then
+          vim.cmd("Copilot enable")
+        end
+      end,
+    })
+
+    -- Create a command to toggle Copilot
+    vim.api.nvim_create_user_command("CopilotToggle", function()
+      if vim.g.copilot_enabled == true then
+        vim.cmd("Copilot disable")
+        print("Copilot disabled")
+      else
+        vim.cmd("Copilot enable")
+        print("Copilot enabled")
+      end
+    end, { nargs = 0 })
+
+    -- Map a key to toggle Copilot
+    vim.keymap.set("n", "<leader>cp", "<Cmd>CopilotToggle<CR>", {
+      silent = true,
+      desc = "Toggle Copilot",
     })
   end,
 }
