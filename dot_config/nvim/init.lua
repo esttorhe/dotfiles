12e47d0cd673adeef9cfea2ee4e4a21de14c31ae
Esteban Torres <me+github@estebantorr.es>
2025-10-16 13:40:18 +0200

    feat(ui): enable relative line numbers for improved navigation in Neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,4 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.wo.relativenumber = true -- Show relative line numbers
 vim.opt.guioptions:remove("T") -- Remove toolbar
